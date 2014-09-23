98da9c2141cce0233e3b7dade21d99a6f969e7c0
CodyAlanSanders <cody.alan.sanders@gmail.com>
2014-09-23 10:45:41 -0400

    Employee Functionality Framework
    Created the Primary Employee Controllers with links between their
    actions.
    
    Front-End: You now have basic views to start working within. This should
    allow to start laying out how things will work.
    
    Back-End: Begin coding the handle data input as well as accepting files.
    Create more links within the Views to expand functionality.
    
    Database: You can begin preparing corresponding tables to accept data.

---
+++ rfp-mgmt-system/rfp-mgmt-system/Controllers/EmployeeRFIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rfp_mgmt_system.Controllers
+{
+    public class EmployeeRFIController : Controller
+    {
+        //
+        // GET: /EmployeeRFI/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+	}
+}
---
+++ rfp-mgmt-system/rfp-mgmt-system/Controllers/EmployeeRFPController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rfp_mgmt_system.Controllers
+{
+    public class EmployeeRFPController : Controller
+    {
+        //
+        // GET: /EmployeeRFP/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+	}
+}
