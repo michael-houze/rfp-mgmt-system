be3da827564fb23d5cb635453f9d1f97d62b3f7f
Michael Houze <michael.houze@gmail.com>
2014-09-25 13:57:59 -0400

    Build Controller Framework for Vendor Functions
    - Added Controllers for vendor functions
    - Added Views for vendor functions
    - Created Action links for workflow

---
+++ rfp-management-system/Controllers/VendorRFIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rfp_mgmt_system.Controllers
+{
+    public class VendorRFIController : Controller
+    {
+        // GET: VendorRFI
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Respond()
+        {
+            return View();
+        }
+
+        public ActionResult Decline()
+        {
+            return View();
+        }
+    }
+}
---
+++ rfp-management-system/Controllers/VendorRFPController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rfp_mgmt_system.Controllers
+{
+    public class VendorRFPController : Controller
+    {
+        // GET: VendorRFP
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Respond()
+        {
+            return View();
+        }
+
+        public ActionResult Decline()
+        {
+            return View();
+        }
+    }
+}
