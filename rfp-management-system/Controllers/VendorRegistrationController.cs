e2874bf529f6e2b44318c05e266c7dce9935d41b
Michael Houze <michael.houze@gmail.com>
2014-10-02 12:45:25 -0400

    Begin Vendor Registration Process
    - Removed duplicate model classes
    - Removed Employee Management controller (They has dependancies on
    duplicate models)
    - Added Vendor Registration based on Vendor model.  Will need to
    finalize This ERD section to proceed.

---
+++ rfp-management-system/Controllers/VendorRegistrationController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using rfp_mgmt_system.Models;
+
+namespace rfp_mgmt_system.Controllers
+{
+    public class VendorRegistrationController : Controller
+    {
+        private BaptistHealthEntities db = new BaptistHealthEntities();
+
+        // GET: VendorRegistration
+        public ActionResult Index()
+        {
+            var vENDORs = db.VENDORs.Include(v => v.VENDOR_LOGIN);
+            return View(vENDORs.ToList());
+        }
+
+        // GET: VendorRegistration/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VENDOR vENDOR = db.VENDORs.Find(id);
+            if (vENDOR == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vENDOR);
+        }
+
+        // GET: VendorRegistration/Create
+        public ActionResult Create()
+        {
+            ViewBag.Vendor_ID = new SelectList(db.VENDOR_LOGIN, "Vendor_ID", "Vendor_Username");
+            return View();
+        }
+
+        // POST: VendorRegistration/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Vendor_ID,VendorFirstName,VendorLastName,VendorPhone,VendorEmail,CompanyName,Street,City,State,Zip,Sanctioned")] VENDOR vENDOR)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VENDORs.Add(vENDOR);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Vendor_ID = new SelectList(db.VENDOR_LOGIN, "Vendor_ID", "Vendor_Username", vENDOR.Vendor_ID);
+            return View(vENDOR);
+        }
+
+        // GET: VendorRegistration/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VENDOR vENDOR = db.VENDORs.Find(id);
+            if (vENDOR == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Vendor_ID = new SelectList(db.VENDOR_LOGIN, "Vendor_ID", "Vendor_Username", vENDOR.Vendor_ID);
+            return View(vENDOR);
+        }
+
+        // POST: VendorRegistration/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Vendor_ID,VendorFirstName,VendorLastName,VendorPhone,VendorEmail,CompanyName,Street,City,State,Zip,Sanctioned")] VENDOR vENDOR)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vENDOR).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Vendor_ID = new SelectList(db.VENDOR_LOGIN, "Vendor_ID", "Vendor_Username", vENDOR.Vendor_ID);
+            return View(vENDOR);
+        }
+
+        // GET: VendorRegistration/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VENDOR vENDOR = db.VENDORs.Find(id);
+            if (vENDOR == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vENDOR);
+        }
+
+        // POST: VendorRegistration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VENDOR vENDOR = db.VENDORs.Find(id);
+            db.VENDORs.Remove(vENDOR);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
