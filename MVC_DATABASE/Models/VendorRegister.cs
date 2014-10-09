a9247de47ad83460b6572a37c59ff30eed047d9c
jshields91 <jeshie02@louisville.edu>
2014-10-09 19:04:33 -0400

    Reset Project to Jordan's Sourcecode
    - Godspeed

---
+++ MVC_DATABASE/Models/VendorRegister.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_DATABASE.Models
+{
+    public class VendorRegister
+    {
+       public RegisterViewModel RegisterViewModel{get; set;}
+       public VENDOR VENDOR { get; set; }
+       public List<VENDORCONTACT> VENDORCONTACTLIST { get; set; }
+       public List<PRODUCTCATEGORY> PRODUCTCATEGORYLIST { get; set; }
+    }
+}
--- rfp-management-system/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rfp_management_system.MvcApplication" Language="C#" %>
